ebfbf1c79c338d056ffcd26f66c9c81ebaafbf7f
KhaiNguyen273 <luuphungkhainguyen2732005@gmail.com>
2025-10-05 22:08:37 +0700

    models bus student route and hardcode data

---
+++ backend/models/Route.js
@@ -0,0 +1,127 @@
+// models/Route.js
+const routes = [
+  {
+    id: "T01",
+    start: "Quận 1",
+    end: "Trường THPT A",
+    departTime: "06:30",
+    stops: [
+      "Bến xe Miền Đông cũ",
+      "Nhà thờ Đức Bà",
+      "Công viên 30/4",
+      "Trường THPT A",
+    ],
+  },
+  {
+    id: "T02",
+    start: "Quận 2",
+    end: "Trường THPT A",
+    departTime: "06:40",
+    stops: [
+      "Khu đô thị Thủ Thiêm",
+      "Hầm Thủ Thiêm",
+      "Cầu Sài Gòn",
+      "Công viên Lê Văn Tám",
+      "Trường THPT A",
+    ],
+  },
+  {
+    id: "T03",
+    start: "Quận 3",
+    end: "Trường THPT A",
+    departTime: "06:45",
+    stops: [
+      "Ngã sáu Dân Chủ",
+      "Công viên Tao Đàn",
+      "Chợ Bến Thành",
+      "Trường THPT A",
+    ],
+  },
+  {
+    id: "T04",
+    start: "Quận 4",
+    end: "Trường THPT A",
+    departTime: "06:35",
+    stops: [
+      "Cầu Ông Lãnh",
+      "Đường Khánh Hội",
+      "Cảng Nhà Rồng",
+      "Cầu Calmette",
+      "Trường THPT A",
+    ],
+  },
+  {
+    id: "T05",
+    start: "Quận 5",
+    end: "Trường THPT A",
+    departTime: "06:50",
+    stops: [
+      "Bến xe Chợ Lớn",
+      "Đại học Sư Phạm TP.HCM",
+      "Công viên Tao Đàn",
+      "Trường THPT A",
+    ],
+  },
+  {
+    id: "T06",
+    start: "Quận 6",
+    end: "Trường THPT A",
+    departTime: "06:55",
+    stops: [
+      "Aeon Mall Bình Tân",
+      "Đường Hậu Giang",
+      "Chợ Lớn",
+      "Trường THPT A",
+    ],
+  },
+  {
+    id: "T07",
+    start: "Quận 7",
+    end: "Trường THPT A",
+    departTime: "06:40",
+    stops: [
+      "Crescent Mall",
+      "Đại học Tôn Đức Thắng",
+      "Cầu Kênh Tẻ",
+      "Trường THPT A",
+    ],
+  },
+  {
+    id: "T08",
+    start: "Quận 8",
+    end: "Trường THPT A",
+    departTime: "06:25",
+    stops: [
+      "Bến xe Quận 8",
+      "Đường Tạ Quang Bửu",
+      "Cầu Chữ Y",
+      "Trường THPT A",
+    ],
+  },
+  {
+    id: "T09",
+    start: "Quận 9",
+    end: "Trường THPT A",
+    departTime: "06:20",
+    stops: [
+      "Khu công nghệ cao",
+      "Vincom Thủ Đức",
+      "Ngã tư Thủ Đức",
+      "Trường THPT A",
+    ],
+  },
+  {
+    id: "T10",
+    start: "Quận 10",
+    end: "Trường THPT A",
+    departTime: "06:15",
+    stops: [
+      "Công viên Lê Thị Riêng",
+      "Siêu thị BigC Miền Đông",
+      "Cầu vượt Ba Tháng Hai",
+      "Trường THPT A",
+    ],
+  },
+];
+
+module.exports = routes;
